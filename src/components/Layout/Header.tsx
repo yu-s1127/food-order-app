3df312688b2f68618d571b33f516f37f4b8ad9f7
yuhei_suzuki <yooopopo5@gmail.com>
2022-01-15 23:26:56 +0900

    カートコンポーネントの追加

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,5 +1,6 @@
 import { FC, Fragment } from 'react'
 
+import HeaderCartButton from './HeaderCartButton'
 import mealsImage from '../../assets/meals.jpeg'
 import classes from './Header.module.css'
 
@@ -8,7 +9,7 @@ const Header: FC = () => {
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt="A table full of delicious food!" />
